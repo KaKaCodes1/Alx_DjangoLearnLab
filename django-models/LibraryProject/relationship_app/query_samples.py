85392b4e9324239bc3b5e874abd0dbb8847ba3b0
Hamisi_Juma <hamisig123@gmail.com>
2025-11-13 21:23:35 +0300

    removed spaces

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,8 +10,8 @@ def allBooksLibrary(library_name):
 
 #Query all books by a specific author
 def allBooksByAuthor(author_name):
-    author = Author.objects.get(name = author_name)
-    books = Book.objects.filter(author = author)
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
 
     for book in books:
         print(book)
@@ -19,6 +19,6 @@ def allBooksByAuthor(author_name):
 #Retrieve the librarian for a library
 def getlibrarian(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library = library)
+    librarian = Librarian.objects.get(library=library)
 
     print(librarian)
