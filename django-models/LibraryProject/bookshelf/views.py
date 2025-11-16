bb504226a6b8a86155d6937dc5827943edf3e3db
Hamisi_Juma <hamisig123@gmail.com>
2025-11-16 18:11:04 +0300

    Learning how to make function-based views

--- django-models/LibraryProject/bookshelf/views.py
+++ django-models/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+def hello_view(request):
+    return HttpResponse("Hellooo World")
+def book_list(request):
+    books = Book.objects.all()
+    context = {'book_list':books}
+    return render(request, 'books/book_list.html', context)
