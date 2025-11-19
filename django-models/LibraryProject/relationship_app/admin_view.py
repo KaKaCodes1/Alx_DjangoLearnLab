b1fe6acb50a6ddec8b3fddf7e1a805ba9d496628
Hamisi_Juma <hamisig123@gmail.com>
2025-11-19 15:19:54 +0300

    admin for the checker

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 from .models import UserProfile
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
+    return UserProfile.objects.filter(user=user, role='Admin').exists()
 @user_passes_test(is_admin)
 def admin_view(request):
     # context = {'message':'Welcome Admin!'}
