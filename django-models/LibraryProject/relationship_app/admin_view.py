221ae48a7968789d472d5bd776cc849404866f48
Hamisi_Juma <hamisig123@gmail.com>
2025-11-19 15:39:21 +0300

    admin

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.decorators import user_passes_test
-# from django.contrib.auth.models import User
 from django.shortcuts import render
 from .models import UserProfile
-from .views import is_admin
 
-# def is_admin(user):
-#     return UserProfile.objects.filter(user=user, role='Admin').exists()
-# @user_passes_test(is_admin)
-# def Admin(request):
-#     # context = {'message':'Welcome Admin!'}
-#     return render(request, 'relationship_app/admin_view.html')
+def is_admin(user):
+    """Return True if the user is authenticated and has role 'Admin'."""
+    return user.is_authenticated and UserProfile.objects.filter(user=user, role='Admin').exists()
 
 @user_passes_test(is_admin)
-def Admin(request):
-    return render(request, 'relationship_app/admin_view.html', {})
+def admin_view(request):
+    """View only accessible to Admin users."""
+    return render(request, 'relationship_app/admin_view.html')
