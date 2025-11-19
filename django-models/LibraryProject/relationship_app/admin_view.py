dd4392ba62fd3f2fbde7c22689926db529f23cf2
Hamisi_Juma <hamisig123@gmail.com>
2025-11-19 14:44:55 +0300

    Worked on admin view for checker

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -4,13 +4,9 @@ from django.shortcuts import render
 from .models import UserProfile
 
 def is_admin(user):
-    if not user.is_authenticated:
-        return False
-    if not hasattr(user, 'userprofile'):
-        return False
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    context = {'message':'Welcome Admin!'}
-    return render(request, 'relationship_app/admin_view.html', context)
+    # context = {'message':'Welcome Admin!'}
+    return render(request, 'relationship_app/admin_view.html')
