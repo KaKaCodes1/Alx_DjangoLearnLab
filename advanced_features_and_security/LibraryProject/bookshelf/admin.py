a9fd8798d009587c8642af09d3f904eda7358d33
Hamisi_Juma <hamisig123@gmail.com>
2025-11-30 17:09:18 +0300

    worked on the custom user manager and set up the admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year', 'author')
     
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('date_of_birth', 'profile_photo',)
+
+    # Define a new fieldset for the custom fields on the user edit page
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields',{'fields':('date_of_birth', 'profile_photo',)})
+    )
+    ## Defines the layout for adding a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{'fields':('email', 'date_of_birth', 'profile_photo',)})
+    )
+
 # Register your models here.
-admin.site.register(Book, BookAdmin)
+admin.site.register(Book, BookAdmin, CustomUserAdmin, CustomUser)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -11,10 +11,47 @@ class Book(models.Model):
         object_string = f"{self.title} by {self.author} published in {self.publication_year}"
         return object_string
     
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    A custom user manager for the CustomUser model.
+    It provides methods for creating standard users and superusers.
+    """
+    def create_user(self, username, email, password=None,**extra_fields):
+        # 1. Ensure a username is provided (required for AbstractUser by default)
+        if not username:
+            raise ValueError('The username field must be set')
+        if not email:
+            raise ValueError('The email field must be set')
+        
+        # Normalize the email for consistency ie lowercase the domain name 
+        email = self.normalize_email(email)
+
+        #create a user instance
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    # This method is specifically called when running commands like python manage.py createsuperuser
+    def create_superuser(self, username, email, password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True)
     profile_photo = models.ImageField(null=True, blank=True)
+    email = models.EmailField(unique=True)
 
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, username, password):
-#         user = self.model()
+    # Link the CustomUserManager to the model
+    objects = CustomUserManager()
+
