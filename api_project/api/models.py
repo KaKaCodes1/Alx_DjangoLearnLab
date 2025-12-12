9711f284cbd4db4c6f202173338fb1d95c855ff2
Hamisi_Juma <hamisig123@gmail.com>
2025-12-12 16:47:00 +0300

    Building My First API Endpoint with Django REST Framework

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Book:
+class Book(models.Model):
     title = models.CharField(max_length=400)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
