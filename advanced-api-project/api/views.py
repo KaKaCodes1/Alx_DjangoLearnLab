1312630ebcf03566356b49f221d13d068314d75e
Hamisi_Juma <hamisig123@gmail.com>
2025-12-18 18:04:21 +0300

    change permission to IsAuthenticatedOrReadOnly

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .serializers import BookSerializer
 
 # A ListView for retrieving all books.
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticatedOrReadOnly] 
 
 # A DetailView for retrieving a single book by ID.
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticatedOrReadOnly] 
 
 # A CreateView for adding a new book.
 class BookCreateView(generics.CreateAPIView):
