b97b6910bd5ea0904ffb13cb8f67e728a03fe7f7
Hamisi_Juma <hamisig123@gmail.com>
2025-12-18 17:54:31 +0300

    built the generic views

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Book, Author
+from datetime import date
 
 #Serializer for the book model
 class BookSerializer(serializers.ModelSerializer):
@@ -7,18 +8,19 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+    #When you are only validating one field use value rather than data as parameters
+    def validate(self, value):
+        if value > date.today().year:
+            raise serializers.ValidationError("Publication year should not be set in the future")
+        return value
+
 #Serializer for the author model
 #A nested serializer is used to implement the one to many relationship between an author and books
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True)
+    books = BookSerializer(many=True, read_only=True)
     
     class Meta:
         model = Author
-        field = ['name']
+        field = ['name','books']
     
-    def validate(self, data):
-        publication_year = data.get('publication_year')
 
-        if publication_year > 2025:
-            raise serializers.ValidationError("Publication year should not be set in the future")
-        return data
