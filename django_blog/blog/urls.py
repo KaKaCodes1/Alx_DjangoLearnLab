636e51ada33190c00a9e008cef759a5860422b01
Hamisi_Juma <hamisig123@gmail.com>
2025-12-22 20:00:59 +0300

    change url for checker

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 
     #Comments
     # path('posts/<int:pk>/comments/', views.CommentListView.as_view(), name='comment_list'),
-    path('post/<int:pk>/comment/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 
