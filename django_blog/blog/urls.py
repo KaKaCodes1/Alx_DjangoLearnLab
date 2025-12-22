01521f3a27c64c365ab08756f95d8f940539eace
Hamisi_Juma <hamisig123@gmail.com>
2025-12-22 14:51:41 +0300

    update urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
 
 
