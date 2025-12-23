f5e86ea01fcc5efd8edf061987009c570c8c43da
Hamisi_Juma <hamisig123@gmail.com>
2025-12-23 20:25:08 +0300

    fix urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login') ,
     path('profile/',UserProfileView.as_view(), name='user_profile'),
-    path('follow/<int:user_id>', FollowUserView.as_view(),name='follow_user'),
-    path('unfollow/<int:user_id>', UnfollowUserView.as_view(),name='unfollow_user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(),name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(),name='unfollow_user'),
 ]
