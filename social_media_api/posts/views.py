8e792770d62d37b5a7f0a1d32e2f39dce2ad3585
Hamisi_Juma <hamisig123@gmail.com>
2025-12-23 21:15:59 +0300

    add generics

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -40,7 +40,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         
         if not created:
@@ -61,7 +61,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
         
         if like:
