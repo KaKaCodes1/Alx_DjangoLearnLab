7a8af56876a16c62f43768d037f534ce6c600c78
Hamisi_Juma <hamisig123@gmail.com>
2025-12-23 15:28:30 +0300

    worked on the login and register views

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,17 +4,17 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password','bio', 'profile_picture']
 
-    def create(self, validate_data):
-        password = validate_data.pop('password')
+    def create(self, validated_data):
+        password = validated_data.pop('password')
         user = User.objects.create_user(
             password=password,
-            **validate_data
+            **validated_data
         )
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 # Create your views here.
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UserLoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+
+        #user is an instance of CustomUser
+        user = serializer.validated_data['user']
+
+        #generate token or get an existing one
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'username': user.username
+        })
