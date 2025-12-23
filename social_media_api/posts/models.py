d1892e439651a5807509748415569ae6b790597c
Hamisi_Juma <hamisig123@gmail.com>
2025-12-23 20:47:17 +0300

    Like model and notification app

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -32,4 +32,17 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    class Meta:
+        unique_together = ('post', 'user') # Prevents liking the same post twice
 
